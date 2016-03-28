b4c5808ee3cdcd4d39eace44df96ab9836f5b269
Adam Quast <adam.quast@gmail.com>
2016-03-28 11:10:54 -0400

    Fix typo in test name

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVistorTest(FunctionalTest):
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
